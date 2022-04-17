436d038a49ee5dab5a70a0dc2dce9c3409d73196
vit314 <paulahradski@gmail.com>
2022-04-17 10:14:31 +0300

    changes

--- src/Lesson13_14.java
+++ src/Lesson13_14.java
@@ -1,7 +1,17 @@
 public class Lesson13_14 {
     public static void main(String[] args) {
-        for (int i = 1; i < 9; i++){
-            System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
-        }
+//        for (int i = 1; i < 9; i++){
+//            System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
+//        }
+
+
+        System.out.println("I'm trying to print a smiley to the console");
+
+//        System.out.println("\u0030\u0078\u0032\u0036\u0033\u0041");
+
+        System.out.println("\u263a");
+
+//        System.out.println("\u0048\u0065\u0079\u0021 \u0049 \u0066\u0061\u0069\u006c\u0065\u0064 " +
+//                "\u0074\u0068\u0065 \u0074\u0061\u0073\u006b\u002e \u0053\u006f\u0072\u0072\u0079");
     }
 }
