698aac9224b7a484e61f2b387954743c597a8637
vit314 <paulahradski@gmail.com>
2022-04-20 10:04:19 +0300

    changes

---
+++ src/HW_Checking.java
@@ -0,0 +1,13 @@
+public class HW_Checking {
+    public static void main(String[] args) {
+
+
+        for (int i = 40; i <= 60; i = i + 4) {
+            if (i == 48) {
+                System.out.println(i);
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
