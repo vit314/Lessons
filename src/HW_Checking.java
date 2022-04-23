3103cf13b78ea0f1e9569b5a43a60a2c2cb587c7
vit314 <paulahradski@gmail.com>
2022-04-23 12:27:48 +0300

    changes

--- src/HW_Checking.java
+++ src/HW_Checking.java
@@ -1,8 +1,6 @@
 public class HW_Checking {
     public static void main(String[] args) {
 
-
-        // Данный вариант почему то не работает(((
         for (int i = 40; i <= 60; i++)
             if (i % 4 == 0) {
                 System.out.println(i);
