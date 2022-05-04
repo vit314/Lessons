25350e7ab0980dc3e8fa2c71aeeaeba75b9ed4fa
vit314 <paulahradski@gmail.com>
2022-05-04 23:09:39 +0300

    changes

--- src/Lesson20_Exceptions.java
+++ src/Lesson20_Exceptions.java
@@ -2,16 +2,32 @@
 public class Lesson20_Exceptions {
 
     public static void main(String[] args) {
-        checkAge(19);
+
+        checking45();
     }
 
     static void checkAge(int age) {
         if (age < 18) {
             throw new ArithmeticException("Меньше 18 лет - доступ закрыт");
-        }
-        else {
+        } else {
             System.out.println("Старше 18 лет - доступ открыт");
         }
+    }
 
+    public static void checking45() {
+        try {
+            for (int i = 40; i <= 60; i++)
+                if (i % 4 == 0) {
+                    System.out.println(i);
+                    if (i > 45) {
+                        throw new Exception("Число должно быть меньше 45");
+                    }
+                }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
